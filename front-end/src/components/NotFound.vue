8b74faa5423e16f4eae0a0e520cfcdc133ef97e5
svmi3195 <target1983@gmail.com>
2017-08-06 12:56:18 +0300

    added simple 404

---
+++ front-end/src/components/NotFound.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="message">
+    <p>Sorry, we couldn't find the page you requested.</p>
+    <router-link to="home">To Homepage</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+};
+</script>
+
+<style scoped>
+
+.message{
+    padding: 15px;
+}
+
+.message a {
+    padding: 5px;
+    background-color: blue;
+    color: white;
+    border-radius: 5px;
+    text-decoration: none;
+}
+
+</style>
--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Hello from '@/components/Hello';
 import Home from '@/components/Home';
+import NotFound from '@/components/NotFound';
 
 Vue.use(Router);
 
@@ -16,6 +17,14 @@ export default new Router({
       path: '/home',
       name: 'Home',
       component: Home,
+    },
+    {
+      path: '/404',
+      name: '404',
+      component: NotFound,
+    },
+    {
+      path: '/*', redirect: '/404',
     },
   ],
 });
